2d6f8be5dc9840275e9b3cc3f7ac9a209bddd6d9
vilaikulCC <vilaikul@gmail.com>
2022-03-12 22:03:32 +0700

    update Bookitems Component

--- components/Content/BooksItems.vue
+++ components/Content/BooksItems.vue
@@ -1,19 +1,82 @@
 <template>
+  <div class="grid-books">
+    <article v-for="(books, index) in items" :key="index" class="grid-item">
+      <img :src="books._embedded['wp:featuredmedia'][0].source_url" alt="" />
+      <h2>{{ books.title.rendered }}</h2>
+      <!-- <p v-html="books.excerpt.rendered" class="item-excerpt" /> -->
+      <p class="item-author">{{ books.acf.author }}</p>
+      <p class="item-price">{{ Number(books.acf.price).toFixed(2) }}</p>
 
+      <button
+        type="button"
+        class="btn btn-default btn-add-to-cart"
+        v-if="books.acf.status !== 'outOfStock'"
+      >
+        <font-awesome-icon icon="fa-solid fa-basket-shopping" /> Add to Cart
+      </button>
+      <button
+        type="button"
+        class="btn btn-default btn-add-to-cart"
+        v-else-if="books.acf.status === 'outOfStock'"
+        disabled
+      >
+        <font-awesome-icon icon="fa-solid fa-basket-shopping" /> Out of stock
+      </button>
+      <!-- <a :href=""></a> -->
+    </article>
+  </div>
 </template>
 <script lang="ts">
 import Vue from "vue";
-import { Books } from "@/interface";
 
 export default Vue.extend({
-  data() {
-    return {
-      booksList: [] as Books[],
-    };
-  },
-
   props: {
-    items: {} as Object,
+    items: {
+      type: [],
+    },
   },
 });
 </script>
+<style lang="scss" scoped>
+
+.grid-books {
+  display: grid;
+  grid-gap: 20px;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  width: 100%;
+
+  .grid-item {
+    background-color: #ffffff;
+    h2 {
+      text-align: center;
+      padding: 10px 20px 10px;
+    }
+    img {
+      max-width: 100%;
+    }
+    .item-excerpt {
+      padding: 0 20px 10px;
+      ::v-deep p {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+      }
+    }
+    .item-author {
+      text-align: center;
+    }
+
+    .item-price {
+      text-align: center;
+    }
+    .btn-add-to-cart {
+      display: block;
+      margin: 20px auto;
+    }
+    .btn-add-to-cart:disabled {
+    }
+  }
+}
+</style>
--- components/Content/BooksList.vue
+++ components/Content/BooksList.vue
@@ -2,39 +2,15 @@
   <div>
     <h1 class="page-title">All Books</h1>
 
-    <div class="">Total : {{ totalsPage }} Items</div>
-
-    <div class="grid-books">
-      <!-- <BooksItem :items="booksList" /> -->
-      <article
-        v-for="(books, index) in booksList"
-        :key="index"
-        class="grid-item"
-      >
-        <img :src="books._embedded['wp:featuredmedia'][0].source_url" alt="" />
-        <h2>{{ books.title.rendered }}</h2>
-        <!-- <p v-html="books.excerpt.rendered" class="item-excerpt" /> -->
-        <p class="item-author">{{ books.acf.author }}</p>
-        <p class="item-price">{{ Number(books.acf.price).toFixed(2) }}</p>
-
-        <button
-          type="button"
-          class="btn btn-default btn-add-to-cart"
-          v-if="books.acf.status !== 'outOfStock'"
-        >
-          <font-awesome-icon icon="fa-solid fa-basket-shopping" /> Add to Cart
-        </button>
-        <button
-          type="button"
-          class="btn btn-default btn-add-to-cart"
-          v-else-if="books.acf.status === 'outOfStock'"
-          disabled
-        >
-          <font-awesome-icon icon="fa-solid fa-basket-shopping" /> Out of stock
-        </button>
-        <!-- <a :href=""></a> -->
-      </article>
+    <div class="controls-item">
+      <div>Filter: </div>
+      <div>
+        <p>Total : {{ totalsPage }} Items</p>
+        <p />
+      </div>
     </div>
+
+    <BooksItem :items="booksList" />
 
     <BooksItem />
   </div>
@@ -95,45 +71,12 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
-.grid-books {
+.controls-item {
   display: grid;
-  grid-gap: 20px;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
-  width: 100%;
-
-  .grid-item {
-    background-color: #ffffff;
-    h2 {
-      text-align: center;
-      padding: 10px 20px 10px;
-    }
-    img {
-      max-width: 100%;
-    }
-    .item-excerpt {
-      padding: 0 20px 10px;
-      ::v-deep p {
-        overflow: hidden;
-        text-overflow: ellipsis;
-        display: -webkit-box;
-        -webkit-line-clamp: 2;
-        -webkit-box-orient: vertical;
-      }
-    }
-    .item-author {
-      text-align: center;
-    }
-
-    .item-price {
-      text-align: center;
-    }
-    .btn-add-to-cart {
-      display: block;
-      margin: 20px auto;
-    }
-    .btn-add-to-cart:disabled {
-
-    }
+  grid-template-columns: auto auto;
+  margin-bottom: 20px;
+  p {
+    text-align: right;
   }
 }
 </style>
