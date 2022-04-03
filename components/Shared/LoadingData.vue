022771c119f7387a2d76a2788259835132bd0727
vilaikulCC <vilaikul@gmail.com>
2022-04-03 23:39:40 +0700

    update loading component

--- components/Shared/LoadingData.vue
+++ components/Shared/LoadingData.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="loading-data" :class="setLoading">
     <div class="loading-wrapper">
-      <div class="line"></div>
-      <div class="line"></div>
-      <div class="line"></div>
+      <div class="circle"></div>
+      <div class="circle"></div>
+      <div class="circle"></div>
     </div>
   </div>
 </template>
@@ -34,7 +34,6 @@ export default Vue.extend({
   top: 0;
   height: 100%;
   width: 100%;
-  background-color: rgb(255 255 255 / 90%);
 }
 .loading-wrapper {
   display: block;
@@ -44,23 +43,23 @@ export default Vue.extend({
   padding: 20px 20px 20px;
   border-radius: 5px;
 
-  .line {
+  .circle {
     display: inline-block;
-    width: 15px;
-    height: 15px;
-    border-radius: 15px;
+    width: 12px;
+    height: 12px;
+    border-radius: 12px;
     background-color: $accent-color;
   }
 
-  .line:nth-last-child(1) {
+  .circle:nth-last-child(1) {
     animation: loading 0.6s 0.1s linear infinite;
   }
 
-  .line:nth-last-child(2) {
+  .circle:nth-last-child(2) {
     animation: loading 0.6s 0.2s linear infinite;
   }
 
-  .line:nth-last-child(3) {
+  .circle:nth-last-child(3) {
     animation: loading 0.6s 0.3s linear infinite;
   }
 }
